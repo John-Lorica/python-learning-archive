e3cb9d7e64d1371c48cfa5f119c62eb6e48df929
John Lorica <jroderick.0717@gmail.com>
2026-04-23 11:08:52 +0800

    Finished some more tutorials

---
+++ 6_hour_course/Comments.py
@@ -0,0 +1,11 @@
+# Skybidy blue
+# Second
+# and third
+# bad comments say what the code does, wait, but comments are so developers know what the code does. Mosh goes wild.
+print('Sky is blue')
+
+# Calculates and returns the square of a number
+
+
+def square(number):
+    return number * number
